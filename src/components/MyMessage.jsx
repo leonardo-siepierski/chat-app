19d5ed987192b48bd7d3ea39391d9b6c2bf09bf2
leonardo <leonardo.siepierski@outlook.com>
2022-04-15 18:34:22 -0300

    MyMessage and TheirMessage components created and implemented

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,5 +1,5 @@
-function MyMessage({ message }) {
-  if (message?.attachments?.length > 0) {
+const MyMessage = ({ message }) => {
+  if (message.attachments && message.attachments.length > 0) {
     return (
       <img
         src={message.attachments[0].file}
@@ -7,13 +7,14 @@ function MyMessage({ message }) {
         className="message-image"
         style={{ float: 'right' }}
       />
-    )
+    );
   }
-  return(
+
+  return (
     <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50' }}>
       {message.text}
     </div>
-  )
- }
- 
- export default MyMessage;
+  );
+};
+
+export default MyMessage;
