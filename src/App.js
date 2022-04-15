674d290d46e3c35ace3055c3a5b48200ce7d04f6
leonardo <leonardo.siepierski@outlook.com>
2022-04-15 18:33:06 -0300

    Changed app to receive credentials from localstorage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import { ChatEngine } from 'react-chat-engine';
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 
 function App() {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
     <ChatEngine
       height="100vh"
       projectID="1618b07d-7f83-4635-8eeb-4e843e56c2a3"
-      userName="Leonardo"
-      userSecret="123456"
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
